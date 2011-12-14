101973aa99bed6e6ee810789a1272bf11ddc0337
Alejandro Wainzinger <alex.wainzinger@mygengo.com>
2011-12-14 00:38:40 +0000

    Add logger support.

--- bottle_websocket/server.py
+++ bottle_websocket/server.py
@@ -4,6 +4,6 @@ from ws4py.server.geventserver import UpgradableWSGIHandler
 import threading
 
 class GeventWebSocketServer(ServerAdapter):
-    def run(self, handler):
+    def run(self, handler, log = 'default' ):
         if not threading.local is local.local: monkey.patch_all()
-        pywsgi.WSGIServer((self.host, self.port), handler, handler_class=UpgradableWSGIHandler).serve_forever()
+        pywsgi.WSGIServer((self.host, self.port), handler, log = log, handler_class=UpgradableWSGIHandler).serve_forever()
